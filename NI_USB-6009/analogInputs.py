710cc33a600566c7c847c71ffb0c4e858ad177b7
Lukasz Luczak <luczakmtm@gmail.com>
2019-03-04 18:44:29 +0100

    reading two channels simultaneously in one task

--- NI_USB-6009/analogInputs.py
+++ NI_USB-6009/analogInputs.py
@@ -1,5 +1,5 @@
 import numpy
-numpy.set_printoptions(threshold=10000)
+numpy.set_printoptions(threshold=20000)
 
 from PyDAQmx.DAQmxFunctions import *
 from PyDAQmx.DAQmxConstants import *
@@ -10,38 +10,42 @@ import time
 class MultiChannelAnalogInput():
     """Class to create a multi-channel analog input
     
-    Usage: AI = MultiChannelInput(physicalChannel)
-        physicalChannel: a string or a list of strings
-    optional parameter: limit: tuple or list of tuples, the AI limit values
+    Usage: AI = MultiChannelInput(physicalChannels, sampleRate=1000, numberOfSamples=1000, limit=None, reset=False)
+        physicalChannels: a string or a list of strings
+    optional parameter: sampleRate: number
+                        numberOfSamples: number
+                        limit: tuple or list of tuples, the AI limit values
                         reset: Boolean
     Methods:
-        read(name) - return the value of the input name,
-            name: string - port to bo read e.g "Dev1/ai0"
-        readAll() - return a dictionary name:value
+        readSingleChannel(channel) - return array of ridden values from channel,
+            channel: string - channel to bo read e.g "Dev1/ai0"
+        readTwoChannels(channels) - return tuple with two arrays of ridden values from channels,,
+            channels: string - channels to bo read e.g "Dev1/ai0:1"
     """
 
-    def __init__(self, physicalChannel, sampleRate=1000, numberOfSamples=1000, limit=None, reset=False):
+    def __init__(self, physicalChannels, sampleRate=1000, numberOfSamples=1000, limit=None, reset=False):
         self.sampleRate = sampleRate
         self.numberOfSamples = numberOfSamples
+        self.timeout = 10.0
 
-        if type(physicalChannel) == type(""):
-            self.physicalChannel = [physicalChannel]
+        if type(physicalChannels) == type(""):
+            self.physicalChannels = [physicalChannels]
         else:
-            self.physicalChannel  =physicalChannel
-        self.numberOfChannel = physicalChannel.__len__()
+            self.physicalChannels  =physicalChannels
+        self.numberOfChannel = physicalChannels.__len__()
         if limit is None:
-            self.limit = dict([(name, (-10.0,10.0)) for name in self.physicalChannel])
+            self.limit = dict([(name, (-10.0,10.0)) for name in self.physicalChannels])
         elif type(limit) == tuple:
-            self.limit = dict([(name, limit) for name in self.physicalChannel])
+            self.limit = dict([(name, limit) for name in self.physicalChannels])
         else:
-            self.limit = dict([(name, limit[i]) for  i,name in enumerate(self.physicalChannel)])           
+            self.limit = dict([(name, limit[i]) for  i,name in enumerate(self.physicalChannels)])           
         if reset:
-            DAQmxResetDevice(physicalChannel[0].split('/')[0] )
+            DAQmxResetDevice(physicalChannels[0].split('/')[0] )
         self.configure()
 
     def configure(self):
-        taskHandles = dict([(name,TaskHandle(0)) for name in self.physicalChannel])
-        for name in self.physicalChannel:
+        taskHandles = dict([(name,TaskHandle(0)) for name in self.physicalChannels])
+        for name in self.physicalChannels:
             DAQmxCreateTask("",byref(taskHandles[name]))
             DAQmxCreateAIVoltageChan(taskHandles[name],name,"",DAQmx_Val_RSE,
                                      self.limit[name][0],self.limit[name][1],
@@ -49,28 +53,37 @@ class MultiChannelAnalogInput():
             DAQmxCfgSampClkTiming(taskHandles[name],"",self.sampleRate,DAQmx_Val_Rising,DAQmx_Val_ContSamps,self.numberOfSamples)
         self.taskHandles = taskHandles
 
-    def readAll(self):
-        return dict([(name,self.read(name)) for name in self.physicalChannel])
-
-    def read(self,name = None):
+    def readSingleChannel(self,name = None):
         if name is None:
-            name = self.physicalChannel[0]
+            name = self.physicalChannels[0]
         taskHandle = self.taskHandles[name]
         DAQmxStartTask(taskHandle)
         data = numpy.zeros((self.numberOfSamples,), dtype=numpy.float64)
         read = int32()
-        DAQmxReadAnalogF64(taskHandle,self.numberOfSamples,10.0,DAQmx_Val_GroupByChannel,data,self.numberOfSamples,byref(read),None)
+        DAQmxReadAnalogF64(taskHandle,self.numberOfSamples,self.timeout,DAQmx_Val_GroupByChannel,data,(self.numberOfSamples),byref(read),None)
         DAQmxStopTask(taskHandle)
         return data
+
+    def readTwoChannels(self,name = None):
+        if name is None:
+            name = self.physicalChannels[0]
+        taskHandle = self.taskHandles[name]
+        DAQmxStartTask(taskHandle)
+        data = numpy.zeros((2*self.numberOfSamples,), dtype=numpy.float64)
+        read = int32()
+        DAQmxReadAnalogF64(taskHandle,self.numberOfSamples,self.timeout,DAQmx_Val_GroupByChannel,data,(2*self.numberOfSamples),byref(read),None)
+        DAQmxStopTask(taskHandle)
+        return data[0:self.numberOfSamples-1], data[self.numberOfSamples:]
 
 
 if __name__ == '__main__':
     f = open('result', 'w')
-    multipleAI = MultiChannelAnalogInput(["Dev1/ai0", "Dev1/ai1"], sampleRate=5000, numberOfSamples=5000)
+    multipleAI = MultiChannelAnalogInput(["Dev1/ai0:1", "Dev1/ai2"], sampleRate=5000, numberOfSamples=5000)
     a = time.time()
     for x in range(5):
-        result = multipleAI.read() # 6.5s / 5 = 1.3s
-        # result = multipleAI.readAll() # 13s = 2 inputs x 5 read x 1.3s
+        # średni czas odczytu 1.3s dla NI USB-6009 na maszynie wirtualnej, 1.02s dla symulowanej NI PCIe-6351 na fizycznym komputerze 
+        result = multipleAI.readTwoChannels() # 6.5s / 5 = 1.3s 
+        # result = multipleAI.readSingleChannel("Dev1/ai2") # 6.5s / 5 = 1.3s
     readTime = time.time() - a
     f.write(str(result))
     f.close()
